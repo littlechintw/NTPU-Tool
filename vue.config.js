9f4d53c294b2a7e5382d61d164ba67f60f6aa6f0
littlechin <littlechintw@gmail.com>
2020-05-29 01:08:28 +0800

    [Fix] Fix PWA setting

--- vue.config.js
+++ vue.config.js
@@ -7,13 +7,5 @@ module.exports = {
 module.exports = {
   pwa: {
     name: 'NTPU Tool',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      swSrc: 'dev/sw.js',
-    }
   }
 }
