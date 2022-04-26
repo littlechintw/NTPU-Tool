5be0d63a9d09512e37f3610690db3e65781ea4e4
littlechintw <littlechintw@gmail.com>
2022-04-26 20:00:12 +0800

    Add data type

--- src/views/Covid.vue
+++ src/views/Covid.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
     <br />
-    <h1>Covid-19 in NTPU</h1>
-    <h5>Data from: <a href="http://lms.ntpu.edu.tw/board.php?courseID=50180&f=news" target="_bland">北大防疫專區</a></h5>
+    <h1>Covid-19 @NTPU</h1>
+    <h5>Data from: <a href="http://lms.ntpu.edu.tw/board.php?courseID=50180&f=news" target="_bland">北大防疫專區</a> （資料依校方公告為主）</h5>
     <h3 style="color: red">{{ data_loading }}</h3>
     <v-divider />
     <br />
@@ -80,7 +80,7 @@
               <h0>{{ cdc_data_tidy.confirmedCase }}</h0>
             </v-row>
             <v-row align="center" justify="center" length>
-              <h4>已公告確診人數</h4>
+              <h4>已公告總確診人數</h4>
             </v-row>
           </v-col>
         </v-row>
@@ -137,6 +137,56 @@
     <v-divider />
     <br />
 
+    <v-card class="mx-auto" width="80%" elevation="0">
+      <v-container fluid>
+        <v-row>
+          <v-col cols="12">
+            <v-row align="center" justify="center" length>
+
+              <v-card class="mx-auto" width="50%" elevation="2" color="#76D1F7">
+                <br />
+                <v-container fluid>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-row align="center" justify="center" length>
+                        <h1>{{ cdc_data_tidy.isolateTotal }}</h1>
+                      </v-row>
+                      <v-row align="center" justify="center" length>
+                        <h3>{{ cdc_data_tidy.newestConfirmedCaseDate }} 居家隔離</h3>
+                      </v-row>
+                    </v-col>
+                  </v-row>
+                </v-container>
+                <br />
+              </v-card>
+
+              <v-card class="mx-auto" width="50%" elevation="2" color="#DAF7A6">
+                <br />
+                <v-container fluid>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-row align="center" justify="center" length>
+                        <h1>{{ cdc_data_tidy.selfHealthManagementTotal }}</h1>
+                      </v-row>
+                      <v-row align="center" justify="center" length>
+                        <h3>{{ cdc_data_tidy.newestConfirmedCaseDate }} 自主健康管理</h3>
+                      </v-row>
+                    </v-col>
+                  </v-row>
+                </v-container>
+                <br />
+              </v-card>
+
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+
+    <br />
+    <v-divider />
+    <br />
+
     <!-- <v-card width="80%" height="150px" color="red">
       <h1>{{ cdc_data_tidy.confirmedCase }}</h1>
     </v-card> -->
@@ -161,20 +211,20 @@ export default {
       data_loading: "Loading data...",
       cdc_data: [],
       headers: [
-        { text: '時間', value: 'date' },
-        { text: '總確診', value: 'confirmedCase.total' },
-        { text: '確診 - 臺北', value: 'confirmedCase.zoneDetail.Taipei' },
-        { text: '確診 - 三峽', value: 'confirmedCase.zoneDetail.Sanxia' },
-        { text: '居家隔離 - 學生', value: 'isolate.studentTotal' },
-        { text: '居家隔離 - 學生 - 臺北', value: 'isolate.studentZoneDetail.Taipei' },
-        { text: '居家隔離 - 學生 - 三峽', value: 'isolate.studentZoneDetail.Sanxia' },
-        { text: '居家隔離 - 老師', value: 'isolate.teacher' },
-        { text: '居家隔離 - 職員', value: 'isolate.staff' },
-        { text: '自主健康管理 - 學生	', value: 'selfHealthManagement.studentTotal' },
-        { text: '自主健康管理 - 學生 - 臺北', value: 'selfHealthManagement.studentZoneDetail.Taipei' },
-        { text: '自主健康管理 - 學生 - 三峽', value: 'selfHealthManagement.studentZoneDetail.Sanxia' },
-        { text: '自主健康管理 - 老師', value: 'selfHealthManagement.teacher' },
-        { text: '自主健康管理 - 職員', value: 'selfHealthManagement.staff' },
+        { text: '時間', value: 'date', align: 'center' },
+        { text: '總確診', value: 'confirmedCase.total', align: 'center' },
+        { text: '確診 - 臺北', value: 'confirmedCase.zoneDetail.Taipei', align: 'center' },
+        { text: '確診 - 三峽', value: 'confirmedCase.zoneDetail.Sanxia', align: 'center' },
+        { text: '居家隔離 - 學生', value: 'isolate.studentTotal', align: 'center' },
+        // { text: '居家隔離 - 學生 - 臺北', value: 'isolate.studentZoneDetail.Taipei' },
+        // { text: '居家隔離 - 學生 - 三峽', value: 'isolate.studentZoneDetail.Sanxia' },
+        { text: '居家隔離 - 老師', value: 'isolate.teacher', align: 'center' },
+        { text: '居家隔離 - 職員', value: 'isolate.staff', align: 'center' },
+        { text: '自主健康管理 - 學生	', value: 'selfHealthManagement.studentTotal', align: 'center' },
+        // { text: '自主健康管理 - 學生 - 臺北', value: 'selfHealthManagement.studentZoneDetail.Taipei' },
+        // { text: '自主健康管理 - 學生 - 三峽', value: 'selfHealthManagement.studentZoneDetail.Sanxia' },
+        { text: '自主健康管理 - 老師', value: 'selfHealthManagement.teacher', align: 'center' },
+        { text: '自主健康管理 - 職員', value: 'selfHealthManagement.staff', align: 'center' },
       ],
       cdc_data_tidy: {
         'confirmedCase': 0,
@@ -224,12 +274,16 @@ export default {
       this.cdc_data_tidy.newestConfirmedCase = this.cdc_data[0].confirmedCase.total;
       this.cdc_data_tidy.newestConfirmedCaseTaipei = this.cdc_data[0].confirmedCase.zoneDetail.Taipei;
       this.cdc_data_tidy.newestConfirmedCaseSanxia = this.cdc_data[0].confirmedCase.zoneDetail.Sanxia;
+        this.cdc_data_tidy.isolateTotal += this.cdc_data[0].isolate.studentTotal;
+        this.cdc_data_tidy.isolateTotal += this.cdc_data[0].isolate.teacher;
+        this.cdc_data_tidy.isolateTotal += this.cdc_data[0].isolate.staff;
+        this.cdc_data_tidy.selfHealthManagementTotal += this.cdc_data[0].selfHealthManagement.studentTotal;
+        this.cdc_data_tidy.selfHealthManagementTotal += this.cdc_data[0].selfHealthManagement.teacher;
+        this.cdc_data_tidy.selfHealthManagementTotal += this.cdc_data[0].selfHealthManagement.staff;
       for (var i=0;i<this.cdc_data.length;i++) {
         this.cdc_data_tidy.confirmedCase += this.cdc_data[i].confirmedCase.total;
         this.cdc_data_tidy.confirmedCaseTaipei += this.cdc_data[i].confirmedCase.zoneDetail.Taipei;
         this.cdc_data_tidy.confirmedCaseSanxia += this.cdc_data[i].confirmedCase.zoneDetail.Sanxia;
-        this.cdc_data_tidy.isolateTotal += this.cdc_data[i].isolate.total;
-        this.cdc_data_tidy.selfHealthManagementTotal += this.cdc_data[i].selfHealthManagement.total;
       }
     }
   },
