0123487c61c6f0d1578b5726beaf287bf47f2e22
littlechintw <littlechintw@gmail.com>
2022-05-04 14:23:43 +0800

    Fix serious alert

--- src/components/covidAlert.vue
+++ src/components/covidAlert.vue
@@ -5,13 +5,13 @@
           <v-row align="center" justify="center" length>
             <v-alert dense outlined type="error" max-height="80%">
               <strong>嚴重警告！</strong>
-              目前資料依照 2022/05/02 <strong>原先發佈（數據遭修改前）</strong>之格式發佈，目前不使用累加確診數的方式，但目前公布之數據與前項發生衝突，<strong>資料缺乏正確性</strong>，請自行斟酌校方提供資料，所有內容皆已完全顯示於最下方列表，可參考檢視。
+              目前不使用累加確診數的方式，但發現公布之當日新增數據與前日發生衝突，<strong>資料缺乏正確性</strong>，請自行斟酌校方提供資料，所有內容皆已完全顯示於 <a href="covid/list">防疫數據列表</a>，可參考檢視。
             </v-alert>
           </v-row>
           <v-row align="center" justify="center" length>
             <v-alert dense outlined type="error" max-height="80%">
               <strong>嚴重警告！</strong>
-              由於校方公布之數據在公布過後又突然刪減資訊，目前無法正確得知計算方式，且行政單位說法不一，無法正確判斷所有資料狀況，請謹慎運用此資料。
+              根據 <a href="https://www.dcard.tw/f/ntpu/p/238790018">「本校確診人數統計方式」 - Dcard 臺北大學版</a> 顯示，校方不提供累積確診數之有意義數據，以下數據僅供參考。
             </v-alert>
           </v-row>
         </v-col>
